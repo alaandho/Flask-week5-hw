91ff4cd8281b7e82208f2d56bea3a5f73c76c7e5
Ala Ho <alaandho@gmail.com>
2021-08-03 09:46:20 -0400

    Flask Recipe Site

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Gives access to the project in ANY OS we find ourselves in
+#Allows outside files/folders to be added to the project from the base directory
+
