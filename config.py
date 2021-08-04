a534c6714e43ca06d098b952064824df62c87766
Ala Ho <alaandho@gmail.com>
2021-08-04 03:45:59 -0400

    sign up and db

--- config.py
+++ config.py
@@ -5,3 +5,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #Gives access to the project in ANY OS we find ourselves in
 #Allows outside files/folders to be added to the project from the base directory
 
+
+class Config:
+    SECRET_KEY= "You will never guess..."
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
